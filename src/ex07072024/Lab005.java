53d33883a7387e0f4fef26341883d44cd8b73e49
anujimmy <anuannajimmy@gmail.com>
2024-07-14 13:49:20 +0200

    commit to github

--- src/ex07072024/Lab005.java
+++ src/ex07072024/Lab005.java
@@ -1,12 +1,13 @@
 package ex07072024;
 
-public class Lab004 {
+public class Lab005 {
     public static void main(String[] args) {
-        //Package -> class -> Main Method -> statements(instructions)
         System.out.println(2+2);
         System.out.println(2*2);
         System.out.println(2/2);
         System.out.println(2%2);
         System.out.println(3-2);
+//        throws error when dividing by zero
+        System.out.println(2/0);
     }
 }

485627fe59739580809eb31fbfcbc57f60115ba6
anujimmy <anuannajimmy@gmail.com>
2024-07-23 07:40:42 +0200

    commit to github

---
+++ src/ex07072024/Lab013.java
@@ -0,0 +1,13 @@
+package ex07072024;
+
+public class Lab013 {
+//    can I run class without a main method ?
+    //run option ?
+
+//    public static void main(String[] args) { -> THIS IS THE ENTRY POINT ARGS  HERE IS A OPTIONAL PARAMETER
+//
+//    }
+    public static void main(String[] abc){ //-> args changed to abc but still this works
+
+    }
+}

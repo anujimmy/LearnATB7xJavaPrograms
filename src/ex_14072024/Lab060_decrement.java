da4e13a5cc2d5b1192e31528d6c36e4a61c3698e
anujimmy <anuannajimmy@gmail.com>
2024-07-24 00:14:45 +0200

    new file in package 20 July & decrement operator added

---
+++ src/ex_14072024/Lab060_decrement.java
@@ -0,0 +1,13 @@
+package ex_14072024;
+
+public class Lab060_decrement {
+    public static void main(String[] args) {
+        int b = 10;
+        int c = b--;
+        System.out.println(c);
+        System.out.println(b);
+        System.out.println(b--);
+        System.out.println(c);
+        System.out.println(--b);
+    }
+}

12933798ccde4c0bf9f628cb02491a5fd9f86d6a
anujimmy <anuannajimmy@gmail.com>
2024-07-26 14:38:33 +0200

    Leap year completed

---
+++ src/CodingAssignments/J1_LeapYear.java
@@ -0,0 +1,18 @@
+package CodingAssignments;
+
+import java.util.Scanner;
+
+public class J1_LeapYear {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter a year in YYYY format");
+        int iYear  = sc.nextInt();
+        // iYear %100 is needed for cases like 1800 or 1900
+        // iYear %400 -> multiples of 4 & 100 like 2000
+        if ((iYear % 4 ==0) && (iYear%100 !=0) || (iYear %400 ==0)){
+            System.out.println(iYear + " is a leap year");
+        }else {
+            System.out.println(iYear + " is not a leap year");
+        }
+    }
+}
